ef5854616a354f6615d5ed639bd90cfa17579578
Jesse <jessewilson215@gmail.com>
2018-06-13 10:38:12 -0500

    extra button scale on mouseenter, added jquery hover to welcome page

--- js/imageEnlargeHover.js
+++ js/imageEnlargeHover.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   var $links = $("#body > a");
 
   $links.mouseenter(function() {
-    TweenLite.to(this, .1, {scale: 1.05});
+    TweenLite.to(this, .1, {scale: 1.1});
     });
 
   $links.mouseleave(function() {
