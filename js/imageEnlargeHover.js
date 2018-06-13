22099484ae0e6a30fd153440c37d2b914286f95d
Jesse <jessewilson215@gmail.com>
2018-06-13 10:30:14 -0500

    jquery/Tweenlite hover functionality, removed lightgrey hover color

--- js/imageEnlargeHover.js
+++ js/imageEnlargeHover.js
@@ -3,23 +3,10 @@ $(document).ready(function() {
   var $links = $("#body > a");
 
   $links.mouseenter(function() {
-
-    $(this).animate({
-      width: "25%",
-      height: "25%"},
-      "fast");
-
+    TweenLite.to(this, .1, {scale: 1.05});
     });
 
   $links.mouseleave(function() {
-
-    $(this).animate({
-      width: "20%",
-      height: "20%"},
-      "fast");
-
+  TweenLite.to(this, .1, {scale: 1});
     });
-
-
-
 });
